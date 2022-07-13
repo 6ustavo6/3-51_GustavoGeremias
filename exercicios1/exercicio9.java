00f30929e141939a76ecc54ccf0f45fd703c254a
Gustavo Geremias <104361866+6ustavo6@users.noreply.github.com>
2022-07-13 09:40:27 -0300

    Add files via upload

---
+++ exercicios1/exercicio9.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+public class exercicio9 {  
+    public static void main(String[] args) {
+        float n1, n2, media;
+        Scanner var = new Scanner(System.in);
+        System.out.println("Digite nota1: ");
+        n1 = var.nextFloat();
+        System.out.println("Digite nota2: ");
+        n2 = var.nextFloat();
+        media = n1+n2;
+        JOptionPane.showMessageDialog( null, "media: " + media);
+    }
+}
